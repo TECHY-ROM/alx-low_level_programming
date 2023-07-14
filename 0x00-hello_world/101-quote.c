15dd89d7f3af6e4ad7a2c48e7cf3113f1b9aab7e
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-07-13 17:25:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
+ *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\n");
-	fputs("- Dora Korpar, 2015-10-19", stdout);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
