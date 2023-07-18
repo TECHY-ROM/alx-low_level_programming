878e4826d5b958f7ebf7610589c42892b79a7de0
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-07-18 08:55:27 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that prints alphabet x10
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+}
