b17aec286c08a16136efe4c59da408d832c93608
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-08-05 08:23:06 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - program that multiplies two numbers.
+ * @argc: number count arguments
+ * @argv: argument array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int a;
+	int b;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+
+	return (0);
+}
