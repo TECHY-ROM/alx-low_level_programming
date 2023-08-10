647877cb489c27289bf4fc7403cd9230f6c0b71f
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-08-10 10:20:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *mul(char n, char *num, int num_index, char *dest, int dest_index);
-
-int check_for_digits(char **av);
-
-#include<stdio.h>
-#include <stdlib.h>
-
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -19,5 +12,15 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 int _putchar(char c);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void errors(void);
+
+int _strlen(char *s);
+
+int is_digit(char *s);
 
 #endif
