4777529644aae0f4743940fd0515a04aaa2334eb
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-08-10 08:20:00 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: allocated memory
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98); /* The process will be terminated with status value of 98 */
+	return (ptr);
+}
