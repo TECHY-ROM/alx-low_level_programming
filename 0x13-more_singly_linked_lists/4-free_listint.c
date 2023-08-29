bd4948d60782dc7dc101846bdba17a28632981d7
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-08-29 07:52:08 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - function that frees a listint_t list.
+ * @head: the head of the list
+ *
+ * Return: NULL if it fails
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
