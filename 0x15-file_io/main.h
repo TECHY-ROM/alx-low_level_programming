d618b435882a5846e0802b37c2784f6b826eefc3
TECHY-ROM <adeniyiaremu13@gmail.com>
2023-10-30 15:46:40 -0700

    fileI/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,15 +17,14 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 void check_elf(unsigned char *e_ident);
-void print_error(char *message);
 void print_magic(unsigned char *e_ident);
-void print_class(unsigned char class);
-void print_data(unsigned char data);
-void print_version(unsigned char version);
-void print_osabi(unsigned char osabi);
-void print_abi(unsigned char abi);
-void print_type(unsigned int type);
-void print_entry(unsigned long int entry);
-int main(int argc, char *argv[]);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
